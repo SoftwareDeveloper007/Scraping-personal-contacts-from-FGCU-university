edf7f308bf0d706755cf57a88d311998eeec2488
Wesche D <28940651+ScrapingExpert007@users.noreply.github.com>
2018-02-23 09:12:10 -0800

    2nd Commit

--- main.py
+++ main.py
@@ -12,35 +12,11 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import *
 
 
-def scraping(first_name="", last_name=""):
+def scraping(first_name="", last_name="", driver=None):
 
-    url = "https://directory.fau.edu/"
-    url = url + "dir/send_name?fn={}&ln={}".format(first_name, last_name)
+    url = "http://admin.fgcu.edu/directory/index.aspx"
+    
 
-    r = requests.post(url)
-    html = r.content.decode()
-    soup = BeautifulSoup(html, "html.parser")
-    table = soup.select_one("ul.deptPhoneEmail")
 
-    if table:
-
-        try:
-            # phone_num = soup.select_one("li:nth-of-type(5)").text.strip()
-            phone_num = soup.select_one("i.fa-phone").parent.select_one("a").text.strip()
-        except:
-            phone_num = ""
-        try:
-            email = soup.select_one("i.fa-envelope").parent.select_one("a").text.strip()
-        except:
-            email = ""
-
-    else:
-        phone_num = ""
-        email = ""
-
-    print(phone_num)
-    print(email)
-    return [phone_num, email]
-
-scraping(first_name="STEVEN", last_name="ABBOTT")
+scraping(first_name="SHANNON", last_name="ACOSTA")
 # scraping(first_name="ANTHONY", last_name="ABBATE")
